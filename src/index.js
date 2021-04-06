a82d57f64042886b66f766467a90d6249d06fcca
jackpunt <jpeckj@gmail.com>
2021-04-06 00:00:02 -0700

    commit to git, what we published in npm 1.0.0

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var wspbserver = require("./wspbserver");
+exports["default"] = wspbserver.CnxHandler;
+Object.assign(module.exports, wspbserver.CnxHandler);
+// https://dev.to/charperbonaroo/creating-a-ts-written-npm-package-for-use-in-node-js-or-browser-5gm3 
