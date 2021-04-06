6dd0e76df2b4fbb4099bd9b4c7b1822fcc70ce4f
jackpunt <jpeckj@gmail.com>
2021-04-05 21:25:21 -0700

    include link to how-to site

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import * as wspbserver from "./wspbserver";
 export default wspbserver.CnxHandler;
 Object.assign(module.exports, wspbserver.CnxHandler);
+// https://dev.to/charperbonaroo/creating-a-ts-written-npm-package-for-use-in-node-js-or-browser-5gm3
