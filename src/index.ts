086720f0a4a0a2f0856a76608b926834eb1b2969
jackpunt <jpeckj@gmail.com>
2021-04-24 14:10:25 -0700

    remove EchoServer (and stime exportation)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 // this file is the Barrel:
 export * from "./wspbserver"
-export { stime, CgClient, CgBase } from "wspbclient"
 export * from "./CgServerDriver"
-export * from "./EchoServer"
--- src/wspbserver.ts
+++ src/wspbserver.ts
@@ -5,7 +5,7 @@ import * as dns from "dns";
 import * as ws from "ws";
 import { EzPromise } from "@thegraid/EzPromise";
 import { ServerSocketDriver } from "./ServerSocketDriver";
-import type { AnyWSD, stime, pbMessage } from "wspbclient";
+import type { AnyWSD, pbMessage } from "wspbclient";
 
 
 // Access to ws.WebSocket class! https://github.com/websockets/ws/issues/1517 
@@ -143,5 +143,3 @@ export class WssListener {
 		wsb.connectStream(ws, ...this.drivers)
   }
 }
-
-
