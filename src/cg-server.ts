09fb736637a02349e140f67ec9af114eef11fcaf
jackpunt <jpeckj@gmail.com>
2022-02-28 22:26:01 -0800

    host name on cmdline

--- src/cg-server.ts
+++ src/cg-server.ts
@@ -6,9 +6,9 @@ const cgserver: WSSOpts = {
 	port: 8444,
 	keydir: "/Users/jpeck/keys/"
 }
-
+let host = process.argv[2] || "game7"
 // WssListener injects its own SSD<ws$WebSocket> at the bottom of the stack
-let cnxlp = new WssListener("game7", cgserver, CgServerDriver ).startListening()
+let cnxlp = new WssListener(host, cgserver, CgServerDriver ).startListening()
 cnxlp.then((cnxl) => {
 	console.log("listening %s:%d", cnxl.hostname, cnxl.port)
 }, (reason) => {
