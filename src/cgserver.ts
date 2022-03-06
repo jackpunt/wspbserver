a216418a360f756cd09c7f02f2fd6688c788a821
jackpunt <jpeckj@gmail.com>
2022-03-05 23:27:56 -0800

    one common code to run them all

--- src/cgserver.ts
+++ src/cgserver.ts
@@ -1,9 +1,9 @@
 import { WSSOpts, WssListener, CgServerDriver } from '.';
-import { stime } from '@thegraid/wspbclient';
+import { AnyWSD, stime } from '@thegraid/wspbclient';
 
-export function cgserver() {
-  let host = process.argv.find((val, ndx, ary) => (ndx > 0 && ary[ndx - 1] == "Xname")) || 'game7'
-  let portStr = process.argv.find((val, ndx, ary) => (ndx > 0 && ary[ndx - 1] == "Xport")) || '8444'
+function wssserver(logName: string, driver: new () => AnyWSD, defHost: string, defPort: string) {
+  let host = process.argv.find((val, ndx, ary) => (ndx > 0 && ary[ndx - 1] == "Xname")) || defHost
+  let portStr = process.argv.find((val, ndx, ary) => (ndx > 0 && ary[ndx - 1] == "Xport")) || defPort
   let port = Number.parseInt(portStr)
 
   const svropts: WSSOpts = {
@@ -11,13 +11,13 @@ export function cgserver() {
     port: port,
     keydir: "/Users/jpeck/keys/"
   }
-  console.log(stime(undefined, "cg-server"), "begin", `${host}${svropts.domain}:${svropts.port}`, process.pid)
+  console.log(stime(undefined, logName), "begin", `${host}${svropts.domain}:${svropts.port}`, process.pid)
   // WssListener injects its own SSD<ws$WebSocket> at the bottom of the stack
-  let cnxlp = new WssListener(host, svropts, CgServerDriver).startListening()
+  let cnxlp = new WssListener(host, svropts, driver).startListening()
   cnxlp.then((cnxl) => {
-    console.log(stime(undefined, "cg-server"), `listening ${cnxl.hostname}:${cnxl.port}`, process.pid)
+    console.log(stime(undefined, logName), `listening ${cnxl.hostname}:${cnxl.port}`, process.pid)
   }, (reason) => {
-    console.log(stime(undefined, "cg-server"), "reject:", reason)
+    console.log(stime(undefined, logName), "reject:", reason)
   })
 }
-cgserver()
+wssserver('cgserver', CgServerDriver, 'game7', '8444')
