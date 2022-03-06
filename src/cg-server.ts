b002b62be2f3894d818bce7061cbb52115cc1708
jackpunt <jpeckj@gmail.com>
2022-03-05 18:03:20 -0800

    test wss-listener; with jest 26

--- src/cg-server.ts
+++ src/cg-server.ts
@@ -1,12 +1,15 @@
 import { WSSOpts, WssListener } from './wspbserver';
 import { CgServerDriver } from './CgServerDriver'
 
+let host = process.argv.find((val, ndx, ary) => (ndx>0 && ary[ndx-1] == "Xname")) || 'game7'
+let portStr = process.argv.find((val, ndx, ary) => (ndx>0 && ary[ndx-1] == "Xport")) || '8444'
+let port = Number.parseInt(portStr)
+
 const cgserver: WSSOpts = {
 	domain: ".thegraid.com",
-	port: 8444,
+	port: port,
 	keydir: "/Users/jpeck/keys/"
 }
-let host = process.argv[2] || "game7"
 // WssListener injects its own SSD<ws$WebSocket> at the bottom of the stack
 let cnxlp = new WssListener(host, cgserver, CgServerDriver ).startListening()
 cnxlp.then((cnxl) => {
